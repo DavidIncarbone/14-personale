ea68518fd987cbfeb4b09038a4e7dff93abc40ea
DavidIncarbone <david.incarbone@gmail.com>
2025-05-30 12:39:36 +0200

    Create models with relations

---
+++ school-register/app/Models/Teacher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    /** @use HasFactory<\Database\Factories\TeacherFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+    ];
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class);
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+}
